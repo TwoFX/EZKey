0219c11b9460ef6a57fb3126427f431c6e5be6a5
Markus Himmel <markus@himmel-villmar.de>
2013-04-15 18:15:40 +0200

    Doin some restructuring

---
+++ Source/EZKey/Options.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace EZKey
+{
+    /// <summary>
+    /// Interaction logic for Options.xaml
+    /// </summary>
+    public partial class Options : Window
+    {
+        public Options()
+        {
+            InitializeComponent();
+        }
+
+        private void Slider_ValueChanged_1(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            Manager.Roundness = e.NewValue;
+            Manager.TriggerOptionChanged();
+        }
+    }
+}
